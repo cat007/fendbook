d9033f57e99769bcb561c23318ae512db211dda7
Luv Saxena <luv.saxena@flipkart.com>
2016-02-01 00:41:38 +0530

    routes added

---
+++ appRoot.js
@@ -0,0 +1,23 @@
+var app = angular.module('bookapp', []);
+
+//This configures the routes and associates each route with a view and a controller
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {template: 'partials/home.html'})
+        .otherwise({ redirectTo: '/', template : 'partials/home.html'});
+});
+
+app.controller('loginCtrl',function($scope,$location){
+    $scope.submit = function(){
+        var emailId = $scope.emailId;
+        var password = $scope.password;
+        if($scope.emailId == 'admin@gmail.com' && $scope.password == 'admin' ){
+            $location.path('/tester');
+        }
+
+
+    }
+
+});
+
+
