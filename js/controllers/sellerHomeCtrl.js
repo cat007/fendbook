a5f678e106de60f0f4bf2d5297df751f15e0c8a6
Luv Saxena <luv.saxena@flipkart.com>
2016-02-12 22:27:00 +0530

    changes made

--- js/controllers/sellerHomeCtrl.js
+++ js/controllers/sellerHomeCtrl.js
@@ -46,6 +46,20 @@ app.controller('sellerHomeCtrl',function($scope,loginService){
 	}
 
 	$scope.addBook = function(book){
+
+		var f = document.getElementById('file').files[0],
+      r = new FileReader();
+  r.onloadend = function(e){
+
+    var data = e.target.result;
+    //send you binary data via $http or $resource or do anything else with it
+    console.log("WHats the image");
+    console.log(data);
+
+  }
+  r.readAsBinaryString(f);
+
+
 		console.log("lets check the book");
 		console.log(book);
 		console.log($scope.book);
