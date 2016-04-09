0a4e896cb8abc84687a6de91b27686228dd5f099
Rahul Kumar Singh <rahul.sk@flipkart.com>
2016-04-09 19:33:35 +0530

    get books from db

--- js/controllers/customerCtrl.js
+++ js/controllers/customerCtrl.js
@@ -1,6 +1,19 @@
-app.controller('successfulUploadCtrl',function($scope,$location,$rootScope,bookService,loginService){
+app.controller('customerCtrl',function($scope,$location,$rootScope,bookService,loginService){
 
 	console.log("Are we here before ");
 	console.log("Finally !!!");
-	
+	var user_id = 1;
+    console.log("User id = ");
+    console.log(user_id);
+
+    		bookService.getAllBooksForUserId(user_id,$scope).then(function(response){
+			console.log("customerCtrl change  1");
+			console.log(response);
+			 $scope.resBooks = response.data;
+             console.log("customerCtrl changed");
+             console.log($scope.resBooks);
+			// $location.path("/viewuploadedBook");
+
+
+		});
 });
